42d4e3b681056eaf9bd28ecda8ec279bdcf8b6d5
yenrakker <yenrakker8@gmail.com>
2024-02-05 09:09:48 +0700

    Dropdown added on Tutorials

--- frontend/src/Components/Layouts/Header.js
+++ frontend/src/Components/Layouts/Header.js
@@ -1,10 +1,20 @@
 // Header.js
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -17,11 +27,32 @@ const Header = () => {
         >
           <MenuIcon />
         </IconButton>
+
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Bitter Gourd Pollination
         </Typography>
-        <Button color="inherit">Tutorials</Button>
+
+        <Button
+        color="inherit"
+        onClick={handleClick}
+        aria-controls="tutorials-menu"
+        aria-haspopup="true"
+      >
+        Tutorials
+      </Button>
+      <Menu
+        id="tutorials-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {/* Add your dropdown menu items related to Tutorials here */}
+        <MenuItem onClick={handleClose}>Tutorial 1</MenuItem>
+        <MenuItem onClick={handleClose}>Tutorial 2</MenuItem>
+        <MenuItem onClick={handleClose}>Tutorial 3</MenuItem>
+      </Menu>
         <Button color="inherit">Tips/Recommendations</Button>
+        
         {/* <Button color="inherit">Menu Item 3</Button> */}
         <IconButton
           size="large"
@@ -29,7 +60,7 @@ const Header = () => {
           color="inherit"
           aria-label="profile"
         >
-          <AccountCircleIcon />
+          <AccountCircleIcon/>
         </IconButton>
       </Toolbar>
     </AppBar>
